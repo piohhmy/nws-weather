6229c8f56ca51f4fee6f33c2e161b8165116ce63
Danny Varner <danny.varner@gmail.com>
2013-12-01 00:22:07 -0800

    Adding mongo cache insert

--- nws/mongo_cache.py
+++ nws/mongo_cache.py
@@ -1,5 +1,16 @@
+from pymongo import MongoClient, GEOSPHERE
+from nws import forecast
+import json
+client = MongoClient()
+db = client.weather_hunter
+db.forecasts.create_index([("loc", GEOSPHERE)])
+
+
 def find(coord, resolution):
 	return None
 
-def insert(coord):
-	pass
+def insert(new_forecast):
+	collection = db.forecasts
+	daily_weather_doc = json.loads(json.dumps(new_forecast.daily_weather, cls=forecast.ForecastSerializer))
+	forecast_doc = {'loc':{'type':'Point', 'coordinates':[float(new_forecast.coordinates.lng), float(new_forecast.coordinates.lat)]}, 'daily_weather':daily_weather_doc}
+	collection.insert(forecast_doc)
