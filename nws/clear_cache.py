440c57fa685cfb73acae6c3fbfaa23e34438d755
Danny Varner <danny.varner@gmail.com>
2013-12-01 21:35:48 -0800

    Adding maintaince script to clear cache

---
+++ nws/clear_cache.py
@@ -0,0 +1,8 @@
+from nws import mongo_cache
+
+def main():
+	mongo_cache.empty()
+
+
+if __name__ == '__main__':
+	main()
--- nws/mongo_cache.py
+++ nws/mongo_cache.py
@@ -6,10 +6,10 @@ import os
 client = MongoClient(os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/weather_hunter'))
 db = client.get_default_database()
 db.forecasts.create_index([("loc", GEOSPHERE)])
+collection = db.forecasts
 
 
 def find(coord, max_distance):
-	collection = db.forecasts
 	for result in collection.find({'loc': {'$near':{'$geometry': {'type':'Point', 'coordinates': [float(coord.lng), float(coord.lat)]}, '$maxDistance':max_distance}}}).limit(1):
 		lng, lat = result["loc"]["coordinates"]
 		fore = forecast.Forecast(forecast.Coordinates(lat, lng))
@@ -19,7 +19,9 @@ def find(coord, max_distance):
 
 
 def insert(new_forecast):
-	collection = db.forecasts
 	daily_weather_doc = json.loads(json.dumps(new_forecast.daily_weather, cls=forecast.ForecastSerializer))
 	forecast_doc = {'loc':{'type':'Point', 'coordinates':[float(new_forecast.coordinates.lng), float(new_forecast.coordinates.lat)]}, 'daily_weather':daily_weather_doc}
 	collection.insert(forecast_doc)
+
+def empty():
+	collection.drop()
