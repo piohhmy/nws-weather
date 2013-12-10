b61e535d3ef801c1ad4a68331e61fd191dda826f
Danny Varner <danny.varner@gmail.com>
2013-12-09 18:41:37 -0800

    Fixing test

--- nws/tests/mongo_cache_test.py
+++ nws/tests/mongo_cache_test.py
@@ -1,8 +1,9 @@
 from nose.tools import *
 from nws import mongo_cache as cache_repo
+from nws.forecast import Coordinates
 class TestMongoCache():
 	def test_find_with_no_match(self):
-		coord = (124.2,23.12) 
+		coord = Coordinates(23.12, 124.2) 
 		resolution = 34
 		result = cache_repo.find(coord, resolution)
 		assert_equal(result, None)
