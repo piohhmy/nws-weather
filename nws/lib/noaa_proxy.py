1ad2f2fdc499a9bb69c005011abec0f36fe14a1f
Danny Varner <dvarner@silverspringnet.com>
2017-03-11 15:55:29 -0800

    Remove unused code

--- nws/lib/noaa_proxy.py
+++ nws/lib/noaa_proxy.py
@@ -10,25 +10,6 @@ from lib import dwml_parser
 timeout = 30
 socket.setdefaulttimeout(timeout)
 
-def main():
-    dwml = request_dwml_grid(45.5508, -122.738, 100.0, 100.0, 20.0)
-    parser = dwml_parser.DWML_Parser(dwml)
-    forecast_grid = parser.generate_forecast_grid()
-    print_forecast(forecast_grid)
-
-def request_latlong_list(lat1, lng1, lat2, lng2, resolution):
-    url = build_noaa_url_for_latlng_list(lat1, lng1, lat2, lng2, resolution)
-    f = urllib2.urlopen(url)
-    dwml = f.read()
-    return dwml
-
-
-def request_dwml_grid(lat, lng, lat_distance, lng_distance, resolution=50):
-    url = build_noaa_url_for_weather_grid(lat, lng, lat_distance, lng_distance, resolution)
-    f = urllib2.urlopen(url)
-    dwml = f.read()
-    return dwml
-
 def request_dwml_grid_points(coords):
     url = build_noaa_url_for_weather_grid_points(coords)
     logging.debug("url: %s", url)
@@ -40,26 +21,6 @@ def request_dwml_grid_points(coords):
         raise
     return dwml
 
-def build_noaa_url_for_weather_grid(lat, lng, lat_distance, lng_distance, resolution):
-    noaaURL = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php"
-    paramaters = {"centerPointLat" : lat, \
-                  "centerPointLon" : lng, \
-                  "distanceLat" : lat_distance, \
-                  "distanceLon" : lng_distance, \
-                  "resolutionSquare" : resolution, \
-                  "numDays" : 7}
-    encoded_params = urllib.urlencode(paramaters)
-    nonencoded_params = "&format=24+hourly" # api does not want '+' urlencoded
-    url_query_string = encoded_params + nonencoded_params
-    url = noaaURL + "?" + url_query_string
-    return url
-
-def build_noaa_url_for_latlng_list(lat1, lng1, lat2, lng2, resolution):
-    noaa_url = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php"
-    paramaters = {'listLat1' : lat1, 'listLon1' :lng1, 'listLat2' :lat2, 'listLon2' : lng2, 'resolutionList' : resolution}
-    url_query_string = urllib.urlencode(paramaters)
-    url = noaa_url + "?" + url_query_string
-    return url
 
 def build_noaa_url_for_weather_grid_points(coords):
     comma_delim_coords = ["{},{}".format(coord.lat, coord.lng) for coord in coords]
@@ -71,13 +32,3 @@ def build_noaa_url_for_weather_grid_points(coords):
     url_query_string = encoded_params + nonencoded_params
     url = noaaURL + "?" + url_query_string
     return url
-
-# TODO: make this a member func of forecast_grid?
-def print_forecast(forecast_grid):
-    print "Printing Forecast"
-    for forecast in forecast_grid:
-        print forecast
-        print "\n"
-
-if __name__ == "__main__":
-    main()
